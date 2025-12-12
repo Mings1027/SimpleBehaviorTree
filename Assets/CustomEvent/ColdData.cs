fd4798c58d439c4f14283a73645cb81493522851
Mings1027 <100500113+Mings1027@users.noreply.github.com>
2025-12-12 19:08:05 +0900

    Refactor ColdData class and update event handling

--- Assets/CustomEvent/ColdData.cs
+++ Assets/CustomEvent/ColdData.cs
@@ -4,37 +4,52 @@ using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace PrimeEvent
+namespace CustomEvent
 {
-    internal partial class ColdData
+    internal class ColdData
     {
-        [CanBeNull] internal Action<CustomEventData> invoker;
-        [CanBeNull] internal object invokerCallback;
-        [CanBeNull] internal object invokeTarget;
+        [CanBeNull] internal Action<PooledEventData> Invoker;
+        [CanBeNull] internal object InvokerCallback;
+        [CanBeNull] internal object InvokeTarget;
 
-        internal ref CustomEventData data => ref managedData;
-        internal CustomEventData managedData;
+        [CanBeNull] internal object InvokeObj;
 
-        internal bool oneShot;
+        internal PooledEventData ManagedData;
+
+        internal bool OneShot;
+
+        internal void Clear()
+        {
+            Invoker = null;
+            InvokerCallback = null;
+            InvokeTarget = null;
+            InvokeObj = null;
+            ManagedData.target = null;
+            OneShot = false;
+        }
     }
 
-    [StructLayout(LayoutKind.Explicit, Size = 24)]
-    internal struct CustomEventData
+    [StructLayout(LayoutKind.Explicit, Size = 16)]
+    internal struct PooledEventData
     {
         [FieldOffset(0)] internal object target;
         [FieldOffset(8)] internal ColdData cold;
-        
-        internal void Set<T>([CanBeNull] T _target, [CanBeNull] Action<T> action) where T : class
+
+        internal void Set<T>([CanBeNull] T target, [CanBeNull] Action<T> action) where T : class
         {
             if (action == null) return;
 
-            cold.invokeTarget = _target;
-            cold.invokerCallback = action;
-            cold.invoker = t =>
+            Assert.IsNotNull(cold, "PooledEventData.cold is null. Create() / ReleaseCold()에서 cold 연결을 확인하세요.");
+
+            cold.InvokeTarget = target;
+            cold.InvokerCallback = action;
+            cold.Invoker = t =>
             {
-                var callback = t.cold.invokerCallback as Action<T>;
+                var callback = t.cold.InvokerCallback as Action<T>;
                 Assert.IsNotNull(callback);
-                var callbackTarget = t.cold.invokeTarget as T;
+                
+                var callbackTarget = t.cold.InvokeTarget as T;
+                
                 try
                 {
                     callback(callbackTarget);
@@ -46,6 +61,32 @@ namespace PrimeEvent
             };
         }
 
+        internal void Set<T>([CanBeNull] T target, Action<T, GameObject> action) where T : class
+        {
+            if (action == null) return;
+            Assert.IsNotNull(cold, "PooledEventData.cold is null. Create() / ReleaseCold()에서 cold 연결을 확인하세요.");
+
+            cold.InvokeTarget = target;
+            cold.InvokerCallback = action;
+            cold.Invoker = t =>
+            {
+                var callback = t.cold.InvokerCallback as Action<T, GameObject>;
+                Assert.IsNotNull(callback);
+
+                var callbackTarget = t.cold.InvokeTarget as T;
+                var obj = t.cold.InvokeObj as GameObject;
+
+                try
+                {
+                    callback(callbackTarget, obj);
+                }
+                catch (Exception e)
+                {
+                    t.HandleCallbackException(e);
+                }
+            };
+        }
+        
         void HandleCallbackException(Exception e)
         {
             Debug.LogException(e, target as UnityEngine.Object);
